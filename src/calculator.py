1a19d44d39fcbd2ccce54a4760124998c56509c1
Mirko Morana <m.morana094@gmail.com>
2022-06-06 08:27:31 +0200

    feat: implementing calculator

---
+++ src/calculator.py
@@ -0,0 +1,11 @@
+#
+#  Write a program that given two numbers as input make the main operations.
+#  Output:
+#  Insert first number: 4
+#  Insert second number: 2
+#  SUM: 6
+#  Difference: 2
+#  Multiplication: 8
+# Division
+from typing import union
+def sum(a: [int, float], b:int)
